f7a292819c51daf80df3d500fa24ae67aee4947b
yuhengguo <39503978+yuhengguo@users.noreply.github.com>
2021-10-21 23:47:46 +0800

    game start and background finish

--- app/src/main/java/com/example/flappy_bird_basic/GameView.java
+++ app/src/main/java/com/example/flappy_bird_basic/GameView.java
@@ -1,18 +1,41 @@
 package com.example.flappy_bird_basic;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.View;
 import android.content.Context;
 import android.graphics.Canvas;
+import android.os.Handler;
+import android.view.Display;
+import android.graphics.Point;
 
 public class GameView extends View {
 
     // This is our custom view class
+    Handler handler; // Handler is required to schedule a runnable after some delay
+    Runnable runnable;
+    final int UPDATE_MILLIS = 30;
+    Bitmap background;
+    Display display;
+
 
     public GameView(Context context) {
         super(context);
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                invalidate(); // this will call onDraw
+            }
+        };
+        background = BitmapFactory.decodeResource(getResources(), R.drawable.background);
     }
 
     protected void onDraw(Canvas canvas){
         super.onDraw(canvas);
+        // we will draw our view inside onDraw()
+        // draw the background on canvas
+        canvas.drawBitmap(background, 0, 0, null);
+        handler.postDelayed(runnable, UPDATE_MILLIS);
     }
 }
