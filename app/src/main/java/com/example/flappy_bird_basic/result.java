56234f395f570b3f59950d5e937d67f42ddce245
yuhengguo <39503978+yuhengguo@users.noreply.github.com>
2021-10-28 03:24:57 +0800

    replay feature added to game

--- app/src/main/java/com/example/flappy_bird_basic/result.java
+++ app/src/main/java/com/example/flappy_bird_basic/result.java
@@ -2,13 +2,30 @@ package com.example.flappy_bird_basic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class result extends AppCompatActivity {
+    private ImageButton button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        button = (ImageButton) findViewById(R.id.imageButton2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMainActivity();
+            }
+        });
+    }
+
+    public void openMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
