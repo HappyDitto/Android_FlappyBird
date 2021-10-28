f5cb77255ca75121877ac066003e7b9dbcf142b6
ZuguangTong <810732627@qq.com>
2021-10-28 23:45:21 +0800

    Set and get user's best score

--- app/src/main/java/utils/DatabaseCRUD.java
+++ app/src/main/java/utils/DatabaseCRUD.java
@@ -3,11 +3,13 @@ package utils;
 import static utils.Utils.getFirebaseRef;
 
 import android.app.Activity;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 
 import userInfo.User;
@@ -15,7 +17,7 @@ import userInfo.User;
 public  class DatabaseCRUD {
     private static DatabaseReference dbRef = getFirebaseRef();
     public static void addUser(final Activity aca, final User user){
-        dbRef.child("Users").push().setValue(user);
+        dbRef.child("Users").child(user.getuId()).setValue(user);
 //                .addOnCompleteListener(new OnCompleteListener<Void>() {
 //            @Override
 //            public void onComplete(@NonNull Task<Void> task) {
@@ -29,11 +31,19 @@ public  class DatabaseCRUD {
     }
 
     public static int getUserBestScore(String uId){
-        dbRef.child("User").child(uId).get();
-        return 0;
+        int[] res = {0};
+        dbRef.child("Users").child(uId).child("bestScore").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                Log.i("check best score：", String.valueOf(task.getResult().getValue()));
+                res[0] =  Integer.parseInt(String.valueOf(task.getResult().getValue()));
+            }
+        });;
+        return res[0];
     }
 
-//    public static User getCurrentUser(){
-//
-//    }
+    public static void setUserBestScore(String uId, int score){
+        dbRef.child("Users").child(uId).child("bestScore").setValue(score);
+    }
+
 }
