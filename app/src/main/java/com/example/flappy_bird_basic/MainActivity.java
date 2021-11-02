457a65d5634f0565c2885e9ca0bbcad709bd1fa1
yuhengguo <39503978+yuhengguo@users.noreply.github.com>
2021-11-02 20:06:46 +0800

    discard voice recognition part

--- app/src/main/java/com/example/flappy_bird_basic/MainActivity.java
+++ app/src/main/java/com/example/flappy_bird_basic/MainActivity.java
@@ -17,7 +17,6 @@ import android.view.View;
 import android.content.Intent;
 import android.widget.Button;
 import android.widget.TextView;
-import android.speech.RecognizerIntent;
 import android.widget.Toast;
 
 
